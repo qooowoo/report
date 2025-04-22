02ae4fca50df1e65bc729ab54b1cbd9ad17cc524
qooowoo <chjoo91@gmail.com>
2025-04-22 17:40:46 +0900

    Update script.js
    날짜를 과업분류로 변경

--- public/script.js
+++ public/script.js
@@ -1,10 +1,9 @@
-
 function addEntry() {
   const name = document.getElementById("name").value;
-  const date = document.getElementById("date").value;
+  const category = document.getElementById("category").value;
   const task = document.getElementById("task").value;
 
-  if (!name || !date || !task) {
+  if (!name || !category || !task) {
     alert("모든 항목을 입력해주세요!");
     return;
   }
@@ -14,16 +13,16 @@ function addEntry() {
 
   newRow.innerHTML = `
     <td>${name}</td>
-    <td>${date}</td>
+    <td>${category}</td>
     <td>${task}</td>
     <td><button onclick="deleteRow(this)">삭제</button></td>
   `;
 
-  // 입력값 초기화
   document.getElementById("name").value = '';
-  document.getElementById("date").value = '';
+  document.getElementById("category").value = '';
   document.getElementById("task").value = '';
 }
+
 
 function deleteRow(btn) {
   const row = btn.parentNode.parentNode;
